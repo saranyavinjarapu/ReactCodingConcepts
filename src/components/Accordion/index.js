d17e5ec79f1db2147034802217c037a7f318a8b2
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-03-24 12:38:14 +0530

    setup base structure for accordion title information

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import data from "./data";
 
 const Accordion = () => {
-  return <div>Accordion</div>;
+  return (
+    <div className="wrapper">
+      <div className="Accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No Data Found</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Accordion;
