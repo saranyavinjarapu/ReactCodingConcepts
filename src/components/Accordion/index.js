d760bde49f46088d53b3709de8768d1a24b50f3b
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-03-24 12:50:44 +0530

    added logic to dsiplay answer when clicked on the question title and also hide the answer once reclicked on the existing displayed answer's title

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 
 const Accordion = () => {
+  const [selected, setSelected] = useState(null);
+  const handleSingleSelection = (currentDataItemSelectedId) => {
+    /*currentDataItemSelectedId === selected ? null : currentDataItemSelectedId to make the content disappear 
+    when we reclick on the same header or else it would do nothing when we reclick on the already answer showing title*/
+    setSelected(
+      currentDataItemSelectedId === selected ? null : currentDataItemSelectedId
+    );
+  };
   return (
     <div className="wrapper">
       <div className="Accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-              <div className="title">
+              <div
+                className="title"
+                onClick={() => handleSingleSelection(dataItem.id)}
+              >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
+                {selected === dataItem.id ? (
+                  <div className="answer">{dataItem.answer}</div>
+                ) : null}
               </div>
             </div>
           ))
