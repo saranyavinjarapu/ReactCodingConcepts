9062ee620756c29999e0ff58f1ba91aeb8c11569
saranya vinjarapu <68265309+saranyavinjarapu@users.noreply.github.com>
2024-03-25 12:54:42 +0530

    created button to enable multi selection and modified necessary styles

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -13,6 +13,7 @@ const Accordion = () => {
   };
   return (
     <div className="wrapper">
+      <button>Enable Multi Selection</button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
